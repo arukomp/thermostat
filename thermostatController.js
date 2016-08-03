8eb017d36d6013fcf60e4dca9236df63974cb0f0
Giancarlo <gfabrizi@gmail.com>
2016-08-03 17:14:22 +0200

    cleaned up

--- thermostatController.js
+++ thermostatController.js
@@ -3,50 +3,26 @@ $(document).ready(function(){
 
   var thermostat = new Thermostat();
 
-  // var switchPowerSavingModeOff = function() {
-  //   thermostat.switchPowerSavingModeOff();
-  //   $('#powerMode').text('off');
-  // };
-
   $('#powerModeOff').click(function() {
     thermostat.switchPowerSavingModeOff();
     $('#powerMode').text('off');
   });
-
-  // var switchPowerSavingModeOn = function() {
-  //   thermostat.switchPowerSavingModeOn();
-  //   $('#powerMode').text('on');
-  // };
 
   $('#powerModeOn').click(function() {
     thermostat.switchPowerSavingModeOn();
     $('#powerMode').text('on');
   });
 
-  // var increaseTemperature = function () {
-  //   thermostat.increaseTemperature();
-  //   updateTemp();
-  // };
-
   $('#temperatureUp').click(function(){
     thermostat.increaseTemperature();
     updateTemp();
   });
-
-  // var decreaseTemperature = function () {
-  //   thermostat.decreaseTemperature();
-  //   updateTemp();
-  // };
 
   $('#temperatureDown').click(function(){
     thermostat.decreaseTemperature();
     updateTemp();
   });
 
-  // var reset = function () {
-  //   thermostat.reset();
-  //   updateTemp();
-  // };
   $('#temperatureReset').click(function() {
     thermostat.reset();
     updateTemp();
