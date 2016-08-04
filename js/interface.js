29b1555e7ad18fd6f2754e8891d4424d43e88336
ogirginc <ogirginc@gmail.com>
2016-08-04 08:45:43 +0100

    Add jQuery&Ajax

--- js/interface.js
+++ js/interface.js
@@ -1,4 +1,55 @@
+// interface.js
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat._temperature);
-})
+  updateTemperature();
+
+  $('#temp-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('#temp-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temp-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#psm-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving').text('on')
+      updateTemperature();
+  });
+
+  $('#psm-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving').text('off')
+      updateTemperature();
+  });
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+    $('#current-temperature').text(data.main.temp);
+  })
+
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  }
+
+});
